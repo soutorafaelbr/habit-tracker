cef5e680aea0c1a1bad290ab15fd74bc86ee72e0
Rafael Souto <rafael.borges@possible.com>
2020-06-25 15:46:49 -0300

    added response class to unauthorized requests

---
+++ app/Http/Responses/UnauthorizedResponse.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Http\Responses;
+
+use Illuminate\Http\JsonResponse;
+
+class UnauthorizedResponse extends JsonResponse
+{
+    private int $code = 401;
+
+    private string $message = 'Unauthorized credentials for the target resource';
+
+    public function __construct()
+    {
+        parent::__construct(
+            [
+                'error' => $this->message
+            ],
+            $this->code
+        );
+    }
+
+}
