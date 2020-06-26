eee461f8db8434bfe48213d95aab990972999471
Rafael Souto <rafael.borges@possible.com>
2020-06-26 09:13:36 -0300

    added middleware auth:api to protected routes

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,16 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\GoalController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('register', RegisterController::class);
 Route::post('login', LoginController::class);
 
+Route::group([
+    'middleware' => 'auth:api',
+], function () {
+
+    Route::get('goals', [GoalController::class, 'index']);
+
+});
