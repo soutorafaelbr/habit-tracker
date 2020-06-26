8bf4cf2a842553858d963c90ffaf35439854ae3c
Rafael Souto <rafael.borges@possible.com>
2020-06-26 09:13:57 -0300

    added relation between a goal and a user

--- database/factories/GoalFactory.php
+++ database/factories/GoalFactory.php
@@ -3,10 +3,15 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Goal;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(Goal::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => fn() => factory(User::class)->create()->id,
+        'title' => $faker->sentence,
+        'type' => $faker->word,
+        'fulfill_until' => $faker->dateTime,
+        'start_at' => $faker->dateTime,
     ];
 });
--- database/migrations/2020_06_23_173136_create_goals_table.php
+++ database/migrations/2020_06_23_173136_create_goals_table.php
@@ -15,6 +15,10 @@ class CreateGoalsTable extends Migration
     {
         Schema::create('goals', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
             $table->string('title');
             $table->string('type'); // esporte, leitura, estudo
             $table->datetime('fulfill_until');
